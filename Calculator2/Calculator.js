2bae3b7685a1f7fa68020b2f0fba97329bc64120
Pavani Boppana <152681623+Pavaniboppana@users.noreply.github.com>
2024-06-22 22:13:29 +0530

    Add files via upload
    commit

---
+++ Calculator2/Calculator.js
@@ -0,0 +1,28 @@
+// CALCULATOR PROGRAM  js
+let input = document.getElementById('inputBox');
+let button = document.querySelectorAll('button');
+
+let string = "";
+let arr = Array.from(button);
+
+arr.forEach(button => {
+    button.addEventListener('click', (e) => {
+        if (e.target.innerHTML == "=") {
+            try {
+                string = eval(string);
+            } catch {
+                string = "Error";
+            }
+            input.value = string;
+        } else if (e.target.innerHTML == "AC") {
+            string = "";
+            input.value = string;
+        } else if (e.target.innerHTML == "DEL") {
+            string = string.substring(0, string.length - 1);
+            input.value = string;
+        } else {
+            string += e.target.innerHTML;
+            input.value = string;
+        }
+    });
+});
